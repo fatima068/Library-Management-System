58243c898091e1e12343fdc56bdf9c394d24b951
fatima <wasimfatima4@gmail.com>
2025-04-12 12:36:05 +0500

    master: 12 36 pm

--- main.cpp
+++ main.cpp
@@ -25,3 +25,17 @@ int main () {
     // }
 }
 
+// Add all edited files to Git and commit them
+    // Hit Ctrl + Shift + A (PC) / Cmd + Shift + A (Mac).
+    // Enter the commit message.
+    // Press ENTER.
+// Add ONLY the current file to Git and commit it
+    // Hit Ctrl + Shift + Z (PC) / Cmd + Shift + Z (Mac).
+    // Enter the commit message.
+    // Press ENTER.
+// Setup the auto-prefill for commit messages
+    // Hit Ctrl + Shift + P (PC) / Cmd + Shift + P (Mac).
+// Look for Preferences: Open User Settings.
+// Look for gaac. settings to customize them.
+// Push local commits
+// Hit Ctrl + Shift + X (PC) / Cmd + Shift + X (Mac).
